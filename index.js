076e79ab893a98fa99a0f0da6b105d3111fdcc43
Saket Prasad <saket98838@gmail.com>
2024-03-09 17:04:05 +0530

    Commit initial files

---
+++ index.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push, onValue, remove } from "firebase/database";
+
+const appSettings = {
+    databaseURL: "https://playground-8dfc6-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsements = ref(database, "endorsements")
+
+const textEl = document.getElementById("text-el")
+const btnEl = document.getElementById("btn-el")
+const ulEl = document.getElementById("ul-el")
+
+btnEl.addEventListener("click", function() {
+    let textValue = textEl.value
+    push(endorsements, textValue)
+    clearTextFeild()
+})
+
+onValue(endorsements, function(snapshot) {
+    if (snapshot.exists()) {
+        let endorsementsArray = Object.entries(snapshot.val())
+        clearEndorsements()
+        for (let i = 0; i < endorsementsArray.length; i++) {
+            let currentEndorsement = endorsementsArray[i]
+            addEndorsement(currentEndorsement)
+        }
+    } else {
+        ulEl.innerHTML = "Your words will make a difference!"
+    }
+})
+
+function clearEndorsements() {
+    ulEl.innerHTML = ""
+}
+
+function clearTextFeild() {
+    textEl.value = ""
+}
+
+function addEndorsement(endorsement) {
+    let endorsementId = endorsement[0]
+    let endorsementValue = endorsement[1]
+    let liEl = document.createElement("li")
+    liEl.textContent = endorsementValue
+    liEl.addEventListener("dblclick", function() {
+        let removerThisEndorsement = ref(database, `endorsements/${endorsementId}`)
+        remove(removerThisEndorsement)
+    })
+    ulEl.append(liEl)
+}
